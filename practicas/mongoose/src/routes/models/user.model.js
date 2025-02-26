8cfa6abd444ab90203bd8390d7835d7c01f7bdc2
SastreMax <sastremaximiliano@gmail.com>
2025-02-25 22:28:53 -0300

    haciendo CRUD en routes

--- practicas/mongoose/src/routes/models/user.model.js
+++ practicas/mongoose/src/routes/models/user.model.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-// Definimos la colección de la base de datos
-const userCollection = "alumnos"; // colección en la base de datos 'estudiantes'
-
 const alumnoSchema = new mongoose.Schema({
     // seteo las propiedades que queremos que un usuario tenga en la aplicación
     nombre: {
@@ -14,7 +11,5 @@ const alumnoSchema = new mongoose.Schema({
     curso: String,
 });
 
-// Con mongoose.model genero el modelo funcional de un usuario
-// El primer parámetro es el nombre del modelo (usamos 'Alumno' como nombre) 
-// y el segundo parámetro es el esquema
-export const alumnoModel = mongoose.model('Alumno', alumnoSchema, userCollection);
+const userModel = mongoose.model("alumnos", alumnoSchema);
+export { userModel };
