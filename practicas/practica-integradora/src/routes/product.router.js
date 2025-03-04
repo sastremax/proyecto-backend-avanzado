7e0f32b7e65f40f16949ef2597ec2d59f21124c1
SastreMax <sastremaximiliano@gmail.com>
2025-03-04 15:43:22 -0300

    PRODUCT.ROUTER.JS
    . Se agrega un post y un get en product.router.js

--- practicas/practica-integradora/src/routes/product.router.js
+++ practicas/practica-integradora/src/routes/product.router.js
@@ -4,13 +4,36 @@ import ProductModel from '../models/product.model';
 
 const router = Router();
 
-
 // C Create
 router.post('/', async (req, res) => {
-    
+    try {
+        const newProduct = new ProductModel(req.body);
+        console.log('info del body')
+
+        await newProduct.save();
+        res.json({newProduct});
+    } catch (error) {
+        return res.send({ mesagge})
+    }
 })
+
 // R Read
+router.get('/:cod', async (req, res) => {
+    try {
+        const product = await ProductModel.findOne({ cod: req.params.cod });
+        if (!product) {
+            return res.json({ error: "error" });
+        }
+        res.render('product', { product: product.toObject() });
+    } catch (error) {
+        return res.send({ message: "error" });
+    }
+});
 
 // U Update
 
+
 // D Delete
+
+
+export default router;
