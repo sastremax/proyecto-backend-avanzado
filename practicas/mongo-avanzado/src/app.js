a56dd26b6bbf8cce5a16a7dfca60c2a8e3a5f782
SastreMax <sastremaximiliano@gmail.com>
2025-03-04 22:34:49 -0300

    config.js y APP
    . Se completa el archivo app.js
    . Se crea config.js

--- practicas/mongo-avanzado/src/app.js
+++ practicas/mongo-avanzado/src/app.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
+import { MONGO_URI } from './config.js';
 import userModel from './models/users.js';
 
 const enviroment = async() => {
-    await mongoose.connect()
-    let response
-}
+    await mongoose.connect(MONGO_URI)
+
+    const response = await userModel.find().explain('executionStats');
+    console.log(response);
+};
+
+environment();
 
 
 
