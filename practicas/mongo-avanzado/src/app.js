29297f007b6390e855bd926fd31eb870835ecc58
SastreMax <sastremaximiliano@gmail.com>
2025-03-04 22:30:56 -0300

    MONGO AVANZADO -gitignore

--- practicas/mongo-avanzado/src/app.js
+++ practicas/mongo-avanzado/src/app.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import userModel from './models/users.js';
 
 const enviroment = async() => {
-    await mongoose
+    await mongoose.connect()
+    let response
 }
 
 
