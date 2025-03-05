84339224f56c56a9e1e6e3edaa376c6dc8831404
SastreMax <sastremaximiliano@gmail.com>
2025-03-05 00:04:00 -0300

    App.js
    . Se agrega dotenv en app

--- practicas/mongo-avanzado/src/app.js
+++ practicas/mongo-avanzado/src/app.js
@@ -1,15 +1,25 @@
 import mongoose from 'mongoose';
-import { MONGO_URI } from './config.js';
+import dotenv from 'dotenv';
 import userModel from './models/users.js';
+import { MONGO_URI } from './config.js';
 
 dotenv.config();
+console.log(' MONGO_URI:', MONGO_URI);
 
-const enviroment = async() => {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('conected');
+const environment = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('conected');
 
-    const response = await userModel.find({ first_name: "Celia" }).explain('executionStats');
-    console.log(response);
+        const response = await userModel.find({ first_name: "Celia" }).explain('executionStats');
+        console.log(response);
+
+        mongoose.connection.close();
+        console.log('disconnect');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+    
 };
 
 environment();
